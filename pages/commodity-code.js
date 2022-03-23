ee9f76830aad4d06752635d0395206a6c0826694
dchatzinaki <61691396+dchatzinaki@users.noreply.github.com>
2022-03-23 18:19:22 +0000

    adding commodity-code

--- pages/commodity-code.js
+++ pages/commodity-code.js
@@ -1,7 +1,9 @@
 import Link from 'next/link'
 import { FormSelect } from "@capgeminiuk/dcx-react-library";
 
-export default function CommodityCode() {
+export default function CommodityCode({data}) {
+
+  console.log('what is data here?', data);
   return (
     <>
       <h1>Select Commodity Code</h1>
@@ -17,10 +19,23 @@ export default function CommodityCode() {
         options={["one", "two", "three"]}
         //onChang
         nullOption="Select..."
-        //  value={selectedState}
-        // containerClassName="form-group"
-        //labelClassName="form-label form-label-bold"
       />
     </>
   )
+}
+
+export async function getServerSideProps(context) {
+    console.log("context from state-presentation");
+    console.log(context.query);
+    const { faoCode, stateCode, presentationCode } = context.query;
+
+    const statePres = await fetch(
+        `http://localhost:9000/v1/commodities/search?speciesCode=${faoCode}&state=${stateCode}&presentation=${presentationCode}`
+    );
+
+    const data = await statePres.json();
+
+ 
+
+    return { props: { data } };
 }
--- pages/state-presentation.js
+++ pages/state-presentation.js
@@ -1,9 +1,28 @@
 import Link from "next/link";
 import { FormSelect } from "@capgeminiuk/dcx-react-library";
+import { useRouter } from 'next/router';
+import { useState } from 'react';
 
-export default function StatePresentationSpecies({ dataTransformed }) {
+export default function StatePresentationSpecies({ faoCode, dataTransformed, action = '/commodity-code' }) {
+    const preventDefault = f => e => {
+        e.preventDefault()
+        f(e)
+    }
+    const router = useRouter()
+    const [query, setQuery] = useState('')
+    const handleParam = setValue => e => setValue(e.target.value)
 
-    var statePresNames = dataTransformed.map(v => `${v.state.description}(${v.state.code}), ${v.presentation.description} (${v.presentation.code})` );
+    const handleSubmit = preventDefault(() => {
+        router.push({
+            pathname: action,
+            query: { faoCode, stateCode: query.split(',')[0], presentationCode: query.split(',')[1] },
+        })
+    })
+
+    var statePresNames = dataTransformed.map(v => ({
+        label: `${v.state.description}(${v.state.code}), ${v.presentation.description} (${v.presentation.code})`,
+        value: `${v.state.code},${v.presentation.code}`
+    }));
     return (
         <>
             <h1>Select State and Presentation</h1>
@@ -12,22 +31,23 @@ export default function StatePresentationSpecies({ dataTransformed }) {
                     <a>Back to Selecting Species</a>
                 </Link>
             </h2>
-            <FormSelect
-                label="State"
-                name="state"
-                id="state"
-                options={statePresNames}
-                nullOption="Select..."
-            />
-            <p>{dataTransformed.length}</p>
+
+            <form onSubmit={handleSubmit}>
+                <FormSelect
+                    label="State"
+                    name="state"
+                    id="state"
+                    options={statePresNames}
+                    nullOption="Select..."
+                    onChange={handleParam(setQuery)}
+                />
+                <button type="submit">Next</button>
+            </form>
         </>
     );
 }
 
 export async function getServerSideProps(context) {
-    console.log("context");
-    console.log(context.query);
-
     const species = context.query['species-choice'].replace(/.*\(|\).*/g, "");
 
     if (!species) {
@@ -41,7 +61,6 @@ export async function getServerSideProps(context) {
     const data = await statePres.json();
 
     const dataTransformed = data.reduce((preV, cV) => {
-        console.log("cV", cV);
         const y = cV.presentations.map((i) => ({
             state: cV.state,
             presentation: i,
@@ -50,5 +69,5 @@ export async function getServerSideProps(context) {
         return [...preV, ...newObj];
     }, []);
 
-    return { props: { dataTransformed } }
+    return { props: { faoCode: species, dataTransformed } }
 }
